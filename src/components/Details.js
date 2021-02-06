b8f3477b9788b1698f5202c4426c31fc6047c082
Bryan Pitt <Bpitt259@gmail.com>
2021-02-06 10:36:06 +0000

    mappign over options

--- src/components/Details.js
+++ src/components/Details.js
@@ -6,9 +6,41 @@ const Details = () => {
   const product = useStoreState((state) => state.getProduct(id));
 
   if (!product) return null;
+  const { id: productId, name, brand, price, available, weight, options } = product;
+
+  const Select = ({ option, values }) => (
+    <select name={option} id={option}>
+      {values.map((val) => (
+        <option value={val}>{val}</option>
+      ))}
+    </select>
+  );
+
+  const Option = ({ option, value }) => {
+    return (
+      <div>
+        <div>
+          {option}: {typeof value === 'object' ? <Select option={option} values={value} /> : value}
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div>
-      {product.id} {product.name}
+      <div>productId: {productId}</div>
+      <div>name: {name}</div>
+      <div>brand: {brand}</div>
+      <div>price: {price}</div>
+      <div>available: {available ? 'yes' : 'no'}</div>
+      <div>weight: {weight}</div>
+      <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+        {options.map((option) => {
+          return Object.keys(option).map((key) => {
+            return <Option option={key} value={option[key]} />;
+          });
+        })}
+      </div>
     </div>
   );
 };
